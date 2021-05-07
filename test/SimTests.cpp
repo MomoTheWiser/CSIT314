e70f33968affb122b7464fff5838cc5991b938eb
thepancakeman <lhq036@uowmail.edu.au>
2021-05-07 19:27:41 +1000

    Testing

--- test/SimTests.cpp
+++ test/SimTests.cpp
@@ -1,7 +1,35 @@
-#include "../ass2-refactor.h"
+#include "../ass3-refactor.h"
 #include <gtest/gtest.h>
+#include <fstream>
+
+using namespace std;
+
+int openFile(const char* fileName){
+
+    ifstream fin;
+
+    //cerr << "Entering the file name: " << fileName << endl;
+
+    fin.open(fileName);
+
+    if(!fin){
+        cerr << "Error opening file " << fileName << ". Program will exit" << endl;
+        return 0;
+    }
+    return 1;
+}
+
+TEST(True, subTest1){
+    ASSERT_TRUE(true);
+}
 
 TEST(TestCase1, subTest1){
-    Sim sim;
-    sim.run();
+    Simulation sim;
+    //sim.openFile("Ass3.txt");
+    openFile("Ass3.txt");
+}
+
+int main(int argc, char **argv){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
